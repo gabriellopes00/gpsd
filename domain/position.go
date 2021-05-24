1649dba9062a62b7f4b620ab3c9dd82b66a15b67
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-05-24 11:00:36 -0300

    test: create tests for position structure

--- domain/position.go
+++ domain/position.go
@@ -1,33 +1,24 @@
-package main
+package domain
 
 import (
-	"fmt"
+	"errors"
 
 	validator "github.com/asaskevich/govalidator"
 )
 
 type Position struct {
-	Latitude  float64 `json:"latitude" valid:"latitude"`
-	Longitude float64 `json:"longitude" valid:"longitude"`
+	Latitude  float64 `json:"latitude,omitempty" valid:"latitude"`
+	Longitude float64 `json:"longitude,omitempty" valid:"longitude"`
 }
 
-func CreatePosition(latitude, longitude float64) *Position {
-	return &Position{Latitude: latitude, Longitude: longitude}
-}
+func NewPosition(latitude, longitude float64) (*Position, error) {
+	newPosition := Position{Latitude: latitude, Longitude: longitude}
 
-func (p *Position) Validate() error {
-	_, err := validator.ValidateStruct(p)
-	return err
-}
-
-func main() {
-	position := CreatePosition(-24.00097794064844, -46.1797712784327)
-	err := position.Validate()
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-func init() {
 	validator.SetFieldsRequiredByDefault(true)
+	_, err := validator.ValidateStruct(newPosition)
+	if err != nil {
+		return nil, errors.New("invalid latitude or longitude format")
+	}
+
+	return &newPosition, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module gps-worker
 
 go 1.16
 
-require github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+require (
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
+	github.com/stretchr/testify v1.7.0
+)
