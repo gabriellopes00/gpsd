8f3999d921e6a2979f1bdf1747df9039e63a2220
Gabriel Lopes <gabrielluislopes00@gmail.com>
2021-06-01 13:47:57 -0300

    feat: create get radius function

--- usecases/calc/get-radius.go
+++ usecases/calc/get-radius.go
@@ -1,44 +1,20 @@
 package calc
 
 import (
-	"fmt"
-	"gps-worker/domain"
-	"math"
-	"strconv"
+	d "gps-worker/domain"
 )
 
-func truncate(value float64) float64 {
-	result, _ := strconv.ParseFloat(fmt.Sprintf("%.2f", math.Ceil(value*100)/100), 64)
-	return result
-}
+func GetRadius(entrypoint d.Position, positions []d.Position) []d.Position {
+	LatRadius := []float64{entrypoint.Latitude + 0.1, entrypoint.Latitude - 0.1}
+	LngRadius := []float64{entrypoint.Longitude + 0.1, entrypoint.Longitude - 0.1}
 
-func GetRadius(entrypoint domain.Position, positions []domain.Position) []domain.Position {
-	lat := truncate(entrypoint.Latitude)
-	lng := truncate(entrypoint.Longitude)
-
-	LatRadius := []float64{lat + 0.1, lat - 0.1}
-	LngRadius := []float64{lng + 0.1, lng - 0.1}
-
-	fmt.Println(LatRadius, LngRadius)
-
-	var radius []domain.Position
+	var radius []d.Position
 	for _, p := range positions {
-		p.Latitude = truncate(p.Latitude)
-		p.Longitude = truncate(p.Longitude)
-
 		if (p.Latitude >= LatRadius[1] && p.Latitude <= LatRadius[0]) &&
 			(p.Longitude >= LngRadius[1] && p.Longitude <= LngRadius[0]) {
 			radius = append(radius, p)
-
-			if p.Name == "SP" {
-				fmt.Println(p)
-			}
 		}
 	}
-
-	// for _, v := range radius {
-	// 	fmt.Println(v)
-	// }
 
 	return radius
 }
